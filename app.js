4253ee65186e9af5e1e11bc988cc0db8cc3c556c
ahoogerwerf <ahoogerwerf@cfpwood.com>
2017-07-03 09:12:00 -0300

    init

--- app.js
+++ app.js
@@ -1,37 +1,35 @@
-    // app.js  
-      
-    var oracledb = require('oracledb');  
-      
 
-    oracledb.getConnection({  
-         user: "plyfin",  
-         password: "basketball",  
-         connectString: "pprd_plyfin"  
-    }, function(err, connection) {  
-         if (err) {  
-              console.error(err.message);  
-              return;  
-         }  
-         connection.execute( "SELECT PRODUCT_GROUP FROM PRODUCT_GROUPS",  
-         [],  
-         function(err, result) {  
-              if (err) {  
-                   console.error(err.message);  
-                   doRelease(connection);  
-                   return;  
-              }  
-              console.log(result.metaData);  
-              console.log(result.rows);  
-              doRelease(connection);  
-         });  
-    });  
-      
-    function doRelease(connection) {  
-         connection.release(  
-              function(err) {  
-                   if (err) {console.error(err.message);}  
-              }  
-         );  
+var oracledb = require('oracledb');  
+
+
+oracledb.getConnection({  
+    user: "plyfin",
+    password: "basketball",
+   // connectString: "pprd_plyfin"
+    connectString: "(DESCRIPTION = (ADDRESS_LIST = (ADDRESS = (PROTOCOL = TCP)(HOST = ply)(PORT = 1521))) (CONNECT_DATA = (SERVICE_NAME = pprd) ) )"
+}, function (err, connection) {
+    if (err) {
+        console.error(err.message);
+        return;  
     }  
+    connection.execute("SELECT PRODUCT_GROUP FROM PRODUCT_GROUPS",
+        [],
+            function (err, result) {
+        if (err) {
+            console.error(err.message);
+            doRelease(connection);
+            return;
+        }
+        console.log(result.metaData);
+        console.log(result.rows);
+        doRelease(connection);
+    });
+});
 
- 
+function doRelease(connection) {
+    connection.release(
+        function (err) {
+            if (err) {console.error(err.message);}
+        }
+    );
+}
